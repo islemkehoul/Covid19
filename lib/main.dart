21051d7255315d3df9caf7d50f183d809d0150a1
islemkehoul <islemkehoul98@gmail.com>
2020-07-24 01:07:29 +0400

    data refresh fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,88 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 //import 'dart:convert';
 
-
 void main() {
-  runApp(
-    MaterialApp(
-       initialRoute: '/',
-       routes: {
-      // When navigating to the "/" route, build the FirstScreen widget.
-      '/': (context) => Home(),
-      // When navigating to the "/second" route, build the SecondScreen widget.
-      //'/second': (context) => SecondScreen(),
-    },
-    ) 
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
 }
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-    Map result;
-    Map cases ; 
+  Map result;
+  Map cases;
 
-   Future getJasonData() async{
+  Future getJasonData() async {
+    var response =  await http.get("https://api.thevirustracker.com/free-api?global=stats");
 
-    var response = await http.get( "https://thevirustracker.com/free-api?global=stats");
     result = json.decode(response.body);
-    cases = result["results"][0];
-    //print(cases[0]['total_cases']);
-    print(result[0]);
-    
+    setState(() {
+       cases = result["results"][0];
+    cases.remove('source');
+    print(cases);
+    });
+   
   }
 
-  /*@override
-  void initState(){
+ /* @override
+  void initState() {
     super.initState();
+    
   }*/
 
   @override
   Widget build(BuildContext context) {
     getJasonData();
-    print(cases);
+    
     var cards = <Widget>[];
-    cases.forEach((key, value) { 
-
-     cards.add( Card(
-             child: Column(
-              children: <Widget>[
-
-               Text('$key : $value'),
-
-              ],
-               
-             ),
-     )
-          );
-     });
+    cases.forEach((key, value) {
+      cards.add(
+        Card(
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                
+                title: Text('$value',
+                style: TextStyle(
+                  fontSize: 20,
+                ),),
+                subtitle: Text('$key',
+                 style:GoogleFonts.montserrat(
+                   fontSize: 15
+                 ),
+                 ),
+              
+              ),
+              // Text('$key : $value'),
+            ],
+          ),
+        ),
+      );
+    });
     return Scaffold(
-      backgroundColor: Colors.grey[700],
-      body:ListView.builder(
-        itemCount: cases.length,
-        itemBuilder: (BuildContext context, int index) {
+        backgroundColor: Colors.grey[700],
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title :Text('Covid19 Global Statistics ')),
+        body: RefreshIndicator(
+          onRefresh: getJasonData,
+          child: ListView.builder(
+            itemCount: cases.length,
+            itemBuilder: (BuildContext context, int index) {
 
-          return cards[index];
-          },
-        )
-
-    );
+              return cards[index];
+            },
+          ),
+        ));
   }
 }
